6448049870a622447c57e46585caed368d010eee
Bernat Bonda <bondabear@gmail.com>
2019-01-03 13:10:26 +0100

    Missing import

--- src/main/java/com/github/saphyra/exceptionhandling/EnableExceptionHandler.java
+++ src/main/java/com/github/saphyra/exceptionhandling/EnableExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.github.saphyra.exceptionhandling;
 
+import com.github.saphyra.exceptionhandling.configuration.BeanConfig;
+import org.springframework.context.annotation.Import;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,5 +10,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Import(BeanConfig.class)
 public @interface EnableExceptionHandler {
 }
